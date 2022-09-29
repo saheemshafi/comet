a71263c3d34e5b28051724b0bd56f5ec8acb97f0
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2022-09-29 21:04:54 +0530

    categories section modified

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
 })
 export class IndexComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  categories: any[] = [];
+  constructor() {
+    this.categories = [
+      { name: 'music', icon: '' },
+      { name: 'Movies', icon: '' },
+      { name: 'Gaming', icon: '' },
+      { name: 'Education', icon: '' },
+      { name: 'Health', icon: '' },
+      { name: 'News', icon: '' },
+      { name: 'Trending', icon: '' },
+      { name: 'Sports', icon: '' },
+      { name: ' Fashion and beauty', icon: '' },
+      { name: 'Live', icon: '' },
+    ];
   }
 
+  ngOnInit(): void {}
 }
