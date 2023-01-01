68011ddc409293afc99ebd0bfdeb561fbf77c250
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2023-01-02 00:59:28 +0530

    added assets

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: 'comet-multimedia',
+    appId: '1:394705205935:web:698ba7b36b1a38a7ad725a',
+    storageBucket: 'comet-multimedia.appspot.com',
+    apiKey: 'AIzaSyCwK0bzQIPnLpubR8h2U8EfA7n1Je6B5pk',
+    authDomain: 'comet-multimedia.firebaseapp.com',
+    messagingSenderId: '394705205935',
+  },
+  rapidApiKey: '78a2e91349msh70971b6e99345a0p1ff485jsn39683e3ed02e',
+  rapidApiHost: 'youtube-v31.p.rapidapi.com',
+  baseUrl: 'https://youtube-v31.p.rapidapi.com',
+};
