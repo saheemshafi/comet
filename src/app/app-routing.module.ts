dac468655e7a7284dceebef2710a050eb4f398b1
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2023-01-02 18:57:37 +0530

    s

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,6 @@ const routes: Routes = [
   { path: '', component: IndexComponent },
   {
     path: 'd',
-    canActivate: [AuthGuard],
     loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
   },
   {
