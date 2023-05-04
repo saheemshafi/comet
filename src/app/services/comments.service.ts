a47a79dfe571773064526b2403d33cfbcbcb3643
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2023-05-04 17:55:36 +0530

    updated files

--- src/app/services/comments.service.ts
+++ src/app/services/comments.service.ts
@@ -16,27 +16,12 @@ export class CommentsService {
       .append('videoId', videoId)
       .append('maxResults', '100');
 
-    return this.http
-      .get<CommentsApiResponse>(`${environment.baseUrl}/commentThreads`, {
+    return this.http.get<CommentsApiResponse>(
+      `${environment.baseUrl}/commentThreads`,
+      {
         headers: getHeaders(),
         params: params,
-      })
-      .pipe(
-        map((comments) => {
-          const modifiedComments = comments.items.map((comment) => {
-            const urlifiedText = this.urlify(
-              comment.snippet.topLevelComment.snippet.textOriginal
-            );
-            comment.snippet.topLevelComment.snippet.textOriginal = urlifiedText;
-            return comment;
-          });
-          return { ...comments, items: modifiedComments };
-        })
-      );
-  }
-
-  urlify(text: string): string {
-    const urlRegex = /(https?:\/\/[^\s]+)/g;
-    return text.replace(urlRegex, (url) => `<a href="${url}">${url}</a>`);
+      }
+    );
   }
 }
