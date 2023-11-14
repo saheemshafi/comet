7ecf0c1b3a0efc8902adddbdf8dcd4abd99b7197
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2023-11-14 22:48:38 +0530

    chore: `videos-page.component` refactored.

--- src/app/user/pages/videos-page/videos-page.component.ts
+++ src/app/user/pages/videos-page/videos-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subject, switchMap, take, takeUntil, tap } from 'rxjs';
 import { Item } from 'src/app/interfaces/search';
 import { SeoService } from 'src/app/services/seo.service';
 import { VideosService } from 'src/app/services/videos.service';
@@ -16,33 +17,39 @@ export class VideosPageComponent implements OnInit {
     private meta: SeoService,
     private router: Router
   ) {}
+
   videos: Item[] = [];
   category: string | null = 'Coding';
-  query: string | null = null;
   nextPageToken?: string;
   fetched: boolean = false;
+  destroy$: Subject<void> = new Subject();
   ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe((params) => {
-      this.category = <string>params.get('category');
-      if (!this.category) return;
-      this.getVideos();
-    });
-    this.activatedRoute.queryParams.subscribe((params) => {
-      this.query = <string>params['q'];
-      if (!this.query) return;
-      this.getVideos();
-    });
-  }
-
-  getVideos(): void {
-    this.meta.updateMetaData(
-      `Comet multimedia - ${this.category || this.query}`,
-      `Browse ${this.category || this.query} videos on Comet multimedia.`,
-      `https://comet-multimedia.up.railway.app${this.router.url}`,
-      'https://comet-multimedia.up.railway.app/assets/images/comet-og-thumb.jpg'
-    );
-    this.videosService
-      .getVideos(<string>(this.category || this.query))
+    this.activatedRoute.paramMap
+      .pipe(
+        takeUntil(this.destroy$),
+        switchMap((params) =>
+          params.get('category')
+            ? this.activatedRoute.paramMap
+            : this.activatedRoute.queryParamMap
+        ),
+        tap({
+          next: (params) => {
+            const query = params.get('category') || params.get('q');
+            this.category = query;
+            this.meta.updateMetaData(
+              `Comet multimedia - ${query}`,
+              `Browse ${query} videos on Comet multimedia.`,
+              `https://comet-multimedia.up.railway.app${this.router.url}`,
+              'https://comet-multimedia.up.railway.app/assets/images/comet-og-thumb.jpg'
+            );
+          },
+        }),
+        switchMap((params) =>
+          this.videosService.getVideos(
+            params.get('category') || params.get('q') || ''
+          )
+        )
+      )
       .subscribe({
         next: (response) => {
           this.nextPageToken = response.nextPageToken;
@@ -50,10 +57,12 @@ export class VideosPageComponent implements OnInit {
           this.videos = this.filterResponse(response.items);
         },
         error: (err) => {
+          console.log(err);
           this.fetched = true;
         },
       });
   }
+
   filterResponse(videos: Item[]): Item[] {
     return videos.filter(
       (item) =>
@@ -61,13 +70,19 @@ export class VideosPageComponent implements OnInit {
         undefined
     );
   }
+
   fetchNextPage() {
     if (!this.nextPageToken) return;
     this.fetched = false;
-    this.videosService.getNextPage(this.nextPageToken).subscribe((response) => {
-      this.fetched = true;
-      this.nextPageToken = response.nextPageToken ? response.nextPageToken : '';
-      this.videos = this.videos.concat(this.filterResponse(response.items));
-    });
+    this.videosService
+      .getNextPage(this.nextPageToken)
+      .pipe(take(1))
+      .subscribe((response) => {
+        this.fetched = true;
+        this.nextPageToken = response.nextPageToken
+          ? response.nextPageToken
+          : '';
+        this.videos = this.videos.concat(this.filterResponse(response.items));
+      });
   }
 }
